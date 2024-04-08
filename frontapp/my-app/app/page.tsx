afa4c111bd3befe44145bbbc2e53ce1f55be2469
TwinOgre <glad2you@naver.com>
2024-04-08 18:31:58 +0900

    member_login_tsx_enhancement

--- frontapp/my-app/app/page.tsx
+++ frontapp/my-app/app/page.tsx
@@ -1,8 +1,11 @@
-import Image from "next/image";
-
+import Link from "next/link";
 export default function Home() {
+  console.log( )
   return (
     <>
+    홈입니다.
+    <br/>
+    <Link href="/auth/login">로그인창</Link>
     </>
   );
 }
