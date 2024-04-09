e47af41df2f2ea4b5b29ba72666f5086625f07c5
twinogre <glad2you@naver.com>
2024-04-09 21:56:45 +0900

    Ingredient_PATCH_DELETE_POST_feature/3

--- src/main/java/com/proj/Matjalal/domain/ingredient/controller/ApiV1IngredientController.java
+++ src/main/java/com/proj/Matjalal/domain/ingredient/controller/ApiV1IngredientController.java
@@ -5,16 +5,16 @@ import com.proj.Matjalal.domain.article.entity.Article;
 import com.proj.Matjalal.domain.ingredient.entity.Ingredient;
 import com.proj.Matjalal.domain.ingredient.service.IngredientService;
 import com.proj.Matjalal.global.RsData.RsData;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -39,6 +39,17 @@ public class ApiV1IngredientController {
         );
     }
 
+    @GetMapping("/type/{type}")
+    public RsData<IngredientsResponse> getIngredientsByType(@PathVariable("type") String type){
+        List<Ingredient> ingredients = this.ingredientService.getListByType(type);
+        return RsData.of(
+                "SI-2",
+                "타입별 다건 요청 성공",
+                new IngredientsResponse(ingredients)
+        );
+
+    }
+
     @AllArgsConstructor
     @Getter
     public static class IngredientResponse {
@@ -56,5 +67,87 @@ public class ApiV1IngredientController {
                 "%d 번 재료는 존재하지 않습니다.".formatted(id),
                 null
         ));
+    }
+
+    @Data
+    public static class CreateRequest {
+        @NotBlank
+        private String name;
+        @NotBlank
+        private String type;
+    }
+    @AllArgsConstructor
+    @Getter
+    public static class CreateResponce {
+        private final  Ingredient ingredient;
+    }
+
+
+    @PostMapping("")
+    public RsData<CreateResponce> create(@RequestBody CreateRequest createRequest){
+        RsData<Ingredient> createRS = this.ingredientService.create(createRequest.getName(), createRequest.getType());
+        if(createRS.isFail()) return (RsData) createRS;
+
+        return RsData.of(
+                createRS.getResultCode(),
+                createRS.getMsg(),
+                new CreateResponce(createRS.getData())
+        );
+    }
+
+    @Data
+    public static class ModifyRequest {
+        @NotBlank
+        private String name;
+        @NotBlank
+        private String type;
+    }
+    @AllArgsConstructor
+    @Getter
+    public static class ModifyResponce {
+        private final  Ingredient ingredient;
+    }
+
+    @PatchMapping("/{id}")
+    public RsData<ModifyResponce> modifyIngredient(@PathVariable("id") Long id, @Valid @RequestBody ModifyRequest modifyRequest){
+        Optional<Ingredient> optionalIngredient = this.ingredientService.findById(id);
+        if(optionalIngredient.isEmpty()){
+            return RsData.of(
+                    "FI-4",
+                    "%s번 재료는 존재하지 않습니다.".formatted(id),
+                    null
+            );
+        }
+
+        RsData<Ingredient> ingredientRsData =this.ingredientService.modify(optionalIngredient.get(), modifyRequest.getName(), modifyRequest.getType());
+
+        return RsData.of(
+                ingredientRsData.getResultCode(),
+                ingredientRsData.getMsg(),
+                new ModifyResponce(ingredientRsData.getData())
+        );
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class DeleteResponce {
+        private final  Ingredient ingredient;
+    }
+    @DeleteMapping("/{id}")
+    public RsData<DeleteResponce> deleteIngredient(@PathVariable("id") Long id){
+        Optional<Ingredient> optionalIngredient = this.ingredientService.findById(id);
+        if(optionalIngredient.isEmpty()){
+            return RsData.of(
+                    "FI-5",
+                    "%d번 재료가 없어 삭제 실패 했습니다.".formatted(id),
+                    null
+            );
+        }
+        RsData<Ingredient> deletedRsData = this.ingredientService.deleteByIngredient(optionalIngredient.get());
+        return RsData.of(
+                deletedRsData.getResultCode(),
+                deletedRsData.getMsg(),
+                new DeleteResponce(deletedRsData.getData())
+        );
     }
 }
