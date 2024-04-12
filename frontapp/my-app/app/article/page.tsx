8c340cd0e6f3c3f16044272ab8782d7a8817f51f
TwinOgre <glad2you@naver.com>
2024-04-12 14:13:24 +0900

    axios_added2_feature/7

--- frontapp/my-app/app/article/page.tsx
+++ frontapp/my-app/app/article/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { useState, useEffect } from 'react'
 import Link from "next/link";
+import axios from 'axios';
 interface articlesInterface {
   id: number,
   createdDate: string,
