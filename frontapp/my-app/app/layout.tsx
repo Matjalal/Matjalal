d454a349a1778d34b005ff10d3cd60c50ff86766
wintersky2 <emsw747@gmail.com>
2024-04-12 17:18:40 +0900

    missing metadata added

--- frontapp/my-app/app/layout.tsx
+++ frontapp/my-app/app/layout.tsx
@@ -1,9 +1,19 @@
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import "./home.css";
 import Footer from './components/Footer.tsx';
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Matjalal",
+  description: "food custom and Recomandation",
+  icons: {
+    icon: "/favicon-16x16.png",
+  },
+};
+
 
 export default function RootLayout({
   children,
