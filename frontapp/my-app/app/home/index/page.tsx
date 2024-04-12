2626f7f5200a71944aa4a13e115860fa282679ca
wintersky2 <emsw747@gmail.com>
2024-04-12 09:15:53 +0900

    home/index added login form and base route in Frontapp

--- frontapp/my-app/app/home/index/page.tsx
+++ frontapp/my-app/app/home/index/page.tsx
@@ -1,42 +1,46 @@
 'use client'
 
+import LoginForm from "@/app/components/loginForm";
 import Link from "next/link"
 
 export default function HomeIndex() {
 
     return (
         <>
-            <div className="text-6xl flex justify-center my-32">
+            <div className="text-6xl flex justify-center mt-24">
                 <img src="/main-title.svg" alt="MainTitle Image" />
+            </div>
+            <div className="flex justify-center">
+                <LoginForm></LoginForm>
             </div>
             <div className="top-section mb-5">
                 <div className="container flex justify-between">
                     <div className="img-box transform -skew-x-12">
                         <img src="/subway-title.svg" alt="Subway Image" />
-                        <div className="bg-black-box">
-                            <div className="top-box">"서브웨이 메뉴 추천"</div>
+                        <Link href="/subway/articles" className="bg-black-box">
+                            <div className="top-box">서브웨이 조합 추천</div>
                             <div className="bottom-box">
-                                <Link href="/article" className="btn-box">더보기</Link>
+                                <div>바로가기</div>
                             </div>
-                        </div>
+                        </Link>
                     </div>
                     <div className="img-box transform -skew-x-12">
                         <img src="/gongcha-title.svg" alt="Gongcha Image" />
-                        <div className="bg-black-box">
-                            <div className="top-box">공차 메뉴 추천</div>
+                        <Link href="/gongcha/articles" className="bg-black-box">
+                            <div className="top-box">공차 조합 추천</div>
                             <div className="bottom-box">
-                                <Link href="/about" className="btn-box">더보기</Link>
+                                <div>바로가기</div>
                             </div>
-                        </div>
+                        </Link>
                     </div>
                     <div className="img-box transform -skew-x-12">
                         <img src="/randombox-title.svg" alt="Gongcha Image" />
-                        <div className="bg-black-box">
-                            <div className="top-box">랜덤 메뉴 추천</div>
+                        <Link href="/member/login" className="bg-black-box">
+                            <div className="top-box">랜덤 메뉴 추천 받기</div>
                             <div className="bottom-box">
-                                <Link href="/member/login" className="btn-box">더보기</Link>
+                                <div>바로가기</div>
                             </div>
-                        </div>
+                        </Link>
                     </div>
                 </div>
             </div>
--- frontapp/my-app/app/layout.tsx
+++ frontapp/my-app/app/layout.tsx
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import "./home.css";
-import Footer from '../components/Footer';
+import Footer from './components/Footer.tsx';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,9 +14,8 @@ export default function RootLayout({
     <html lang="ko">
       <body className={inter.className}>
         {children}
-        <Footer/>
+        <Footer />
       </body>
     </html>
-
   );
 }
