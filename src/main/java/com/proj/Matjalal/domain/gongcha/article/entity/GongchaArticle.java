864285483cbf934ca719f6786cdbc5b2814749d2
wintersky2 <emsw747@gmail.com>
2024-04-15 10:14:54 +0900

    api permitted and need fix Show article when Delete method

--- src/main/java/com/proj/Matjalal/domain/gongcha/article/entity/GongchaArticle.java
+++ src/main/java/com/proj/Matjalal/domain/gongcha/article/entity/GongchaArticle.java
@@ -1,5 +1,6 @@
 package com.proj.Matjalal.domain.gongcha.article.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.proj.Matjalal.domain.ingredient.entity.Ingredient;
 import com.proj.Matjalal.domain.member.entity.Member;
 import com.proj.Matjalal.global.jpa.BaseEntity;
@@ -10,6 +11,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.hibernate.validator.internal.util.stereotypes.Lazy;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +25,7 @@ public class GongchaArticle extends BaseEntity {
     private String subject;
     private String content;
     @ManyToOne
+    @JsonIgnore //Could not write JSON: failed to lazily initialize a collection: could not initialize proxy - no Session 에러로 일단 jsonignore 처리
     private Member author;
     @ManyToMany // ?????????????????? 모르겠음 List랑 게시글이랑 관계를 어떻게 맺어야 할지
     private List<Ingredient> ingredients = new ArrayList<>();
