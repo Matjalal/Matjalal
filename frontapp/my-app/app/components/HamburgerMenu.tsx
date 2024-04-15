da321fae3d16ec4ab318e61afea8a477c1bde024
wintersky2 <emsw747@gmail.com>
2024-04-15 18:41:34 +0900

    added close-icon and logic

--- frontapp/my-app/app/components/HamburgerMenu.tsx
+++ frontapp/my-app/app/components/HamburgerMenu.tsx
@@ -4,12 +4,22 @@ import Link from 'next/link';
 import { useState } from 'react';
 
 export default function HamburgerMenu() {
-
     const [sidebarOpen, setSidebarOpen] = useState(false);
+
+    // 사이드바 토글 함수
+    const toggleSidebar = () => {
+        setSidebarOpen(!sidebarOpen);
+    };
 
     return (
         <div className="flex">
-            <button><img className="size-10" onClick={() => setSidebarOpen(!sidebarOpen)} src="/menu-icon.svg" alt="Menu Icon Image" /></button>
+            <button onClick={toggleSidebar}>
+                {sidebarOpen ? (
+                    <img className="size-10" src="/close-icon.svg" alt="Close Icon Image" />
+                ) : (
+                    <img className="size-10" src="/menu-icon.svg" alt="Menu Icon Image" />
+                )}
+            </button>
             <div className={`fixed lg:w-64 bg-gray-200 ${sidebarOpen ? 'left-0' : '-left-64'} mt-12 transition-all opacity-90 duration-300 ease-in-out`}
                 style={{ height: '100vh' }}>
                 <ul className='grid grid-cols-1 gap-4'>
