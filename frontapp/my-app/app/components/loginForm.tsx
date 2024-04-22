2003a0b660c750bbb4d248c8d2015aa01492a2ff
wintersky2 <emsw747@gmail.com>
2024-04-22 15:55:20 +0900

    member globaling work done

--- frontapp/my-app/app/components/loginForm.tsx
+++ frontapp/my-app/app/components/loginForm.tsx
@@ -1,32 +1,37 @@
+import { useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
+import api from "../utils/api";
+import { useRouter } from "next/navigation";
 
 export default function LoginForm() {
   const [member, setMember] = useState({ username: '', password: '' })
+  const queryClient = useQueryClient();
+  const router = useRouter();
 
-
-  const loginhandleSubmit = async (e: any) => {
-    e.preventDefault();
-    const response = await fetch("http://localhost:8090/api/v1/members/login", {
-      method: 'POST',
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(member)
-    });
-    if (response.ok) {
-      alert('로그인 성공');
-    } else {
-      alert('로그인 실패');
-    }
-  }
   const handleChange = (e: any) => {
     const { name, value } = e.target;
-    // const name: any = e.target.name;
-    // const value = e.target.value;
     setMember({ ...member, [name]: value });
     console.log({ ...member, [name]: value })
   }
+
+  const login = async (e: any) => {
+    e.preventDefault()
+
+    return await api
+      .post("/members/login", {
+        username: member.username,
+        password: member.password
+      })
+      .then(res => {
+          queryClient.setQueryData(['memberKey'], res.data.data.memberDto);
+          router.replace("/");
+      }
+      )
+      .catch((error)=>{
+        console.log(error);
+      })
+  }
+
 
   return (
     <>
@@ -43,7 +48,7 @@ export default function LoginForm() {
             </div>
           </div>
           <div className="grid gap-2">
-            <button onClick={loginhandleSubmit} className="w-full py-2 px-3 bg-indigo-600 hover:bg-indigo-700 disabled:bg-indigo-300 text-white text-sm font-semibold rounded-md shadow" type="button">로그인</button>
+            <button onClick={login} className="w-full py-2 px-3 bg-indigo-600 hover:bg-indigo-700 disabled:bg-indigo-300 text-white text-sm font-semibold rounded-md shadow" type="button">로그인</button>
             <button className="w-full py-2 px-3 bg-indigo-600 hover:bg-indigo-700 disabled:bg-indigo-300 text-white text-sm font-semibold rounded-md shadow" type="button">회원가입</button>
           </div>
         </div>
--- frontapp/my-app/app/member/login/page.tsx
+++ frontapp/my-app/app/member/login/page.tsx
@@ -1,55 +1,17 @@
 'use client'
 import LoginForm from "@/app/components/loginForm";
 import api from "@/app/utils/api";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
 
 export default function Login() {
-  const [member, setMember] = useState({ username: '', password: '' })
-
-  const handleChange = (e: any) => {
-    const { name, value } = e.target;
-    setMember({ ...member, [name]: value });
-    console.log({ ...member, [name]: value })
-  }
-
-  const login = async (e: any) => {
-    e.preventDefault()
-
-    return await api
-      .post("/members/login", {
-        username: member.username,
-        password: member.password
-      })
-      .then(response => response.data.memberDTO)
-  }
-
-  const { isLoading, error, data } = useQuery({
-    queryKey: ['memberInfo'],
-    queryFn: login
-  });
-
-  if (isLoading) <div>Loading....</div>
-
-  if (error) {
-    console.log(error.message)
-  }
+  const queryClient = useQueryClient();
 
   const handleLogout = async () => {
-    const response = await fetch("http://localhost:8090/api/v1/members/logout", {
-      method: 'POST',
-      credentials: 'include', // 핵심 변경점
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-
-    if (response.ok) {
-      alert("ok")
-    } else {
-      alert("fail")
-    }
+    await api.post('/members/logout');
+    queryClient.setQueryData(['memberKey'], null);
   }
+
   return (
     <>
       <button onClick={handleLogout}>로그아웃</button>
