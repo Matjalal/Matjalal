03a43e55e888a9b047cf181f9b60221ca69b8609
wintersky2 <emsw747@gmail.com>
2024-04-22 17:09:35 +0900

    code format

--- frontapp/my-app/app/about/page.tsx
+++ frontapp/my-app/app/about/page.tsx
@@ -1,34 +1,34 @@
-'use client'
-import { useState, useEffect } from 'react'
-import api from '../utils/api';
+"use client";
+import { useState, useEffect } from "react";
+import api from "../utils/api";
 
 type memberInterface = {
-  id: number,
-  createdDate: string,
-  modifiedDate: string,
-  username: string,
-  email: string
-}
+    id: number;
+    createdDate: string;
+    modifiedDate: string;
+    username: string;
+    email: string;
+};
 export default function About() {
-  const [member, setMember] = useState<memberInterface>();
+    const [member, setMember] = useState<memberInterface>();
 
-  useEffect(() => {
-    api.get("/members/me")
-    .then(response => setMember(response.data.data.memberDTO))
-    .catch (err => {
-      console.log(err)
-    })
-  }, [])
+    useEffect(() => {
+        api.get("/members/me")
+            .then((response) => setMember(response.data.data.memberDTO))
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
 
-  return (
-    <div>
-      <h1>😎소개 페이지 입니다.</h1>
-      아이디: {member?.id} | <br />
-      이름: {member?.username} | <br />
-      이메일: {member?.email} | <br />
-      생성일: {member?.createdDate} |
-      <br />
-      <br />
-    </div>
-  );
+    return (
+        <div>
+            <h1>😎소개 페이지 입니다.</h1>
+            아이디: {member?.id} | <br />
+            이름: {member?.username} | <br />
+            이메일: {member?.email} | <br />
+            생성일: {member?.createdDate} |
+            <br />
+            <br />
+        </div>
+    );
 }
